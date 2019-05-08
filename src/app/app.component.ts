dffb50229874ba73e64f7310453b555f0f566216
123wouter <wouter.wouter@hotmail.com>
2019-05-08 16:16:42 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,17 @@ import { Component } from '@angular/core';
     <p>Aantal uren gestudeerd: <span [textContent]="studietijd"></span></p>
     <p>Aantal uren gelanterfanterd: </p>
     <p>Dagen tot aan de volgende vakantie: <b>{{ 365 - dagen }}</b></p>
-    <img [src]="employeeOfTheWeek.picture">
+    <img [src]="studentendata.picture">
 `,
 styles: [` p { font-family:Calibri,Verdana,sans-serif; font-size:1.2rem; } `],
 })
 
 export class AppComponent  {
   studentnaam = 'Henk de Boer';
-  studietijd
-  dagen
+  studietijd = 5;
+  dagen = 280;
+  studentendata = {
+    picture : 'https://pbs.twimg.com/profile_images/974378148476407809/BwaR7PSs_400x400.jpg',
+    test : 'hoi'
+  }
 }
